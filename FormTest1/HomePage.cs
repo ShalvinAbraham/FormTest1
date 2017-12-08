e654fa3e21ac7f32085f3847559734d371b16e0e
Shalvin <sat4star@gmail.com>
2017-12-08 21:42:30 +0000

    Add project files.

---
+++ FormTest1/HomePage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+
+namespace FormTest1
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public string Title => driver.Title;
+
+        public IWebElement Name => driver.FindElement(By.Name("visitor_name"));
+
+        public IWebElement Email => driver.FindElement(By.Name("visitor_email"));
+
+        public IWebElement Phone => driver.FindElement(By.Name("visitor_phone"));
+
+        public IWebElement Message => driver.FindElement(By.Name("visitor_message"));
+
+        public IWebElement Captcha => driver.FindElement(By.Name("captcha_code"));
+
+        public IWebElement Submit => driver.FindElement(By.Name("submit"));
+
+        public void SubmitForm()
+        {
+            Submit.Click();
+        }
+    }
+}
---
+++ FormTest1/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+
+namespace FormTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMinutes(2);
+            driver.Navigate().GoToUrl("https://satlearn.000webhostapp.com/form.php");
+
+            var homePage = new HomePage(driver);
+
+            homePage.Name.SendKeys("Name1");
+            homePage.Email.SendKeys("mail@domain.com");
+            homePage.Phone.SendKeys("123456");
+            homePage.Message.SendKeys("Message 00001");
+            homePage.Captcha.SendKeys("123");
+            homePage.SubmitForm();
+
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
